83997b8913d92252169af411a689fdfc9fd65a65
SHCanard <36749089+SHCanard@users.noreply.github.com>
2023-05-17 11:36:35 +0200

    Delete powershell directory

--- powershell/dsa_ps_shortcut.ps1
+++
@@ -1,2 +0,0 @@
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "Start-Process powershell -Credential domain\account -ArgumentList '-noprofile -command &{Start-Process "mmc dsa.msc" -verb runas}'"
-#Shortcut should be executed as administrator for this to work (Properties > Shortcut > Advanced)
