4b9f6ad04871741b203c71f4710d5d97f0f12710
SHCanard <36749089+SHCanard@users.noreply.github.com>
2024-05-07 12:51:28 +0200

    Update dsa_ps_shortcut.ps1

--- shortcuts/dsa_ps_shortcut.ps1
+++ shortcuts/dsa_ps_shortcut.ps1
@@ -1,2 +1,5 @@
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "Start-Process powershell -Credential domain\user -ArgumentList '-noprofile -command &{Start-Process "mmc dsa.msc" -verb runas}'"
 #Shortcut should be executed as administrator for this to work (Properties > Shortcut > Advanced)
+
+#Alternative if the above does not work :
+runas.exe /user:domain\user "cmd /c start \"\" mmc %SystemRoot%\system32\dsa.msc"
